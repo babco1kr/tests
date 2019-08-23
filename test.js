68d6b4eff0e87c5f9f6937226af8ec2a57e07bd5
Kyle Babcock <47157069+babco1kr@users.noreply.github.com>
2019-08-23 14:50:19 -0400

    false

--- test.js
+++ test.js
@@ -3,6 +3,6 @@ const { expect } = require('chai');
 describe('It should be true', async() => {
     it('should still be true', async() => {
 
-        expect(2 + 2).to.equal(4);
+        expect(2 + 3).to.equal(4);
     })
 })
